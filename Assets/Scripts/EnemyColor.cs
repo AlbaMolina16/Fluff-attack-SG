259a3f15a454dec7686f2992c0f5c9a36e737f28
Alba Molina <alba16me@gmail.com>
2026-04-24 08:14:29 +0200

    correción sprites de colores (layer)

---
+++ Assets/Scripts/EnemyColor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType { Red, Yellow, Green, Blue }
+
+public class EnemyColor : MonoBehaviour
+{
+    public EnemyType type;
+}
