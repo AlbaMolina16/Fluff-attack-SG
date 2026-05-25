d82dcb543ae6a0a2edb47819d93957770b45dce1
Alba Molina Encinar <alba.molina.encinar@avanade.com>
2026-05-25 19:04:34 +0200

    corrección del script para el borrado de bbdd

--- Database/00_DELETE_BBDD.sql
+++ Database/00_DELETE_BBDD.sql
@@ -5,4 +5,6 @@ DROP TABLE users;
 DROP TABLE scores;
 DROP TABLE difficulties;
 DROP TABLE user_preferences;
+DROP TABLE difficulty_movementtype;
+DROP TABLE movement_type;
 SET FOREIGN_KEY_CHECKS = 1;
