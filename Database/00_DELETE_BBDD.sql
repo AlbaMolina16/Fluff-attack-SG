a8f9d61d434f7f1b7bf6b4d18fd4b48f4afdd023
Alba Molina <alba16me@gmail.com>
2026-06-07 11:45:30 +0200

    correción scrip de borrado bbdd sin user_preferences

--- Database/00_DELETE_BBDD.sql
+++ Database/00_DELETE_BBDD.sql
@@ -4,7 +4,6 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE users;
 DROP TABLE scores;
 DROP TABLE difficulties;
-DROP TABLE user_preferences;
 DROP TABLE difficulty_movementtype;
 DROP TABLE movement_type;
 SET FOREIGN_KEY_CHECKS = 1;
